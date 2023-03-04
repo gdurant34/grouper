3014122557237b8220cec9011da51f5b702eaabe
Grace Durant <gdurant34@gmail.com>
2023-03-04 13:10:13 -0800

    Took out authentication so easier to work in app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    before_action :authorized
-    # skip_before_action :authorized, only: :index
-    def authorized
-        return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
-    end
-
-    def current_user
-        User.find(session[:user_id])
-    end
-    
 end
